1da82908e93a4844feef6142b88b8224e72923c7
alamadcs <alamadcs@gmail.com>
2021-06-16 16:26:44 +0400

    CRUD for all 4 tables

---
+++ tests/Fixture/ArticlesTagsFixture.php
@@ -0,0 +1,49 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * ArticlesTagsFixture
+ */
+class ArticlesTagsFixture extends TestFixture
+{
+    /**
+     * Fields
+     *
+     * @var array
+     */
+    // phpcs:disable
+    public $fields = [
+        'article_id' => ['type' => 'integer', 'length' => null, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null, 'autoIncrement' => null],
+        'tag_id' => ['type' => 'integer', 'length' => null, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null, 'autoIncrement' => null],
+        '_indexes' => [
+            'tag_key' => ['type' => 'index', 'columns' => ['tag_id'], 'length' => []],
+        ],
+        '_constraints' => [
+            'primary' => ['type' => 'primary', 'columns' => ['article_id', 'tag_id'], 'length' => []],
+        ],
+        '_options' => [
+            'engine' => 'MyISAM',
+            'collation' => 'utf8_general_ci'
+        ],
+    ];
+    // phpcs:enable
+    /**
+     * Init method
+     *
+     * @return void
+     */
+    public function init(): void
+    {
+        $this->records = [
+            [
+                'article_id' => 1,
+                'tag_id' => 1,
+            ],
+        ];
+        parent::init();
+    }
+}
